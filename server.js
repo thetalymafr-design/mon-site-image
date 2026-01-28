6812008c521be1d7c7c5f6b7ec8e3b5bb7f6df40
thetalymafr-design <thetalymafr@gmail.com>
2026-01-28 16:51:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,30 +11,12 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/generate", async (req, res) => {
-  try {
-    const { prompt } = req.body;
-
-    const response = await fetch("https://api.openai.com/v1/images/generations", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-      },
-      body: JSON.stringify({
-        model: "gpt-image-1",
-        prompt,
-        size: "512x512",
-        background: "transparent"
-      })
-    });
-
-    const data = await response.json();
-    res.json({ image: data.data[0].url });
-  } catch (err) {
-    res.status(500).json({ error: "Image generation failed" });
-  }
+app.post("/generate", (req, res) => {
+  res.json({
+    image: "/logo.png"
+  });
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on po
+  console.log("Server running on port", PORT);
+});
